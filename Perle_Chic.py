4aea93e7e2327e1784791ce500c77042635a6456
Pedro Klein <79673266+pepeklein@users.noreply.github.com>
2021-04-20 21:11:14 -0300

    Update 3

--- Perle_Chic.py
+++ Perle_Chic.py
@@ -7,18 +7,18 @@
 preco_cm1 = 0.0025
 preco_cm2 = 0.00068
 mdo1 = 3.70
-mdo2 = 4.10
+mdo2 = 5.50
 
 
 def pulseira(cm_pulso, cm_disco):
-    a = preco_cm1 * cm_pulso
+    a = preco_cm1 * (cm_pulso + 11)
     b = (cm_disco + 1) * 0.18
     pf = a + b + mdo1
     return '%.2f' % pf
 
 
 def colar(cm_pescoco, cm_disco):
-    a = preco_cm2 * cm_pescoco
+    a = preco_cm2 * (cm_pescoco + 25)
     b = (cm_disco) * 0.18
     pf = a + b + mdo2
     return '%.2f' % pf
@@ -35,10 +35,11 @@ while True:
     elif opcao == '2':
         cm_pescoco = float(input('Insira os centímetros do pescoço: '))
         cm_disco = float(input('Insira os centímetros do disco: '))
-        print('O custo de produção é: R$', (colar(cm_pescoco, cm_disco)))
+        print('O custo de produção é: R$', colar(cm_pescoco, cm_disco))
         print('O custo de venda é: R$', float(colar(cm_pescoco, cm_disco)) * 3)
         break
     else:
         print('Valor inexistente.')
 
 print('Obrigado por utilizar nosso programa')
+
